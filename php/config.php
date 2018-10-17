0a3f919568557bef40bf13a0794d390ae4f5d658
caloloc2 <calolomino@gmail.com>
2018-10-17 15:44:00 -0500

    modificacion python camara

---
+++ php/config.php
@@ -0,0 +1,6 @@
+<?php
+
+define("HOSTNAME", "localhost");// Nombre del host (localhost aws instance)
+define("USERNAME", "root"); // Nombre del usuario
+define("PASSWORD", " "); // Contraseña
+define("DATABASE", "camara"); // Nombre de la base de datos
