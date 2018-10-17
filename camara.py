1ae3bba6e24a68609466c5a16235dac406344fa1
caloloc2 <calolomino@gmail.com>
2018-10-17 11:36:02 -0500

    modificacion python camara

--- camara.py
+++ camara.py
@@ -2,14 +2,19 @@
 #Importamos librerias necesarias
 import cv2 # para manejo de video y fotos
 import MySQLdb # para manejo de base de datos
-
+db = MySQLdb.connect(host="localhost", user="root", passwd=" ", db="camara")
+cur = db.cursor()
 #funcion para guardar nombre de foto en base de datos
-def guarda_bd(foto):
-	db = MySQLdb.connect(host="localhost", user="root", passwd=" ", db="camara")
-	cur = db.cursor()
+def guarda_bd(foto):	
 	sql = "INSERT INTO fotos (foto) VALUES (%s)"
 	val = (foto,)
 	cur.execute(sql, val)
+	db.commit()
+	db.close()
+
+def inicia():
+	sql = "TRUNCATE TABLE fotos"
+	cur.execute(sql)
 	db.commit()
 	db.close()
 
@@ -17,10 +22,13 @@ camara = 0
 vidcap = cv2.VideoCapture(camara)
 success,image = vidcap.read()
 count = 0
+
+inicia()
+
 while success:
 	success,image = vidcap.read()
 	nombre_foto = "frame%d.jpg" % count
-	cv2.imwrite(nombre_foto, image)
+	cv2.imwrite("fotos/"+nombre_foto, image)
 	guarda_bd(nombre_foto)
 	count += 1
 	if (count>20):
